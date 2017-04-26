9cabd2d6bb8a994dcfce66dc00457d4b671831ea
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2017-04-26 16:14:36 -0400

    Initial Commit

---
+++ RKSnippet/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RKSnippet
+//
+//  Created by Gabriel Le Breton on 2017-04-26.
+//  Copyright © 2017 gableroux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
