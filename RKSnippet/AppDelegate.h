53b8fb8842808b2f3d01f53db9d3922bd39f6289
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2017-04-26 16:21:42 -0400

    Removed useless comments

--- RKSnippet/AppDelegate.h
+++ RKSnippet/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  RKSnippet
-//
-//  Created by Gabriel Le Breton on 2017-04-26.
-//  Copyright © 2017 gableroux. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
